a739975655b471391e9d19ffcddfde85bd4c8782
maxkhramtsov13 <41873877+maxkhramtsov13@users.noreply.github.com>
2023-02-14 17:28:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 //и на выходе показывает вторую цифру этого числа.
 
 Random threeDigitNumber = new Random();
-int number = threeDigitNumber.Next( 10 , 1000);
+int number = threeDigitNumber.Next( 100 , 999);
 Console.WriteLine("Three digit number -> " + number);
 Console.WriteLine("The second digit of a three-digit number -> " + number / 10 % 10);
