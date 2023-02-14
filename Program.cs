3e818544c7db3919d35904c51c372cc120773087
MaxKhramtsov <maxkhramtsov13@gmail.com>
2023-02-14 12:13:49 +0300

    homework01

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа.
+
+Random threeDigitNumber = new Random();
+int number = threeDigitNumber.Next( 10 , 1000);
+Console.WriteLine("Three digit number -> " + number);
+Console.WriteLine("The second digit of a three-digit number -> " + number / 10 % 10);
